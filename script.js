a05de37efa4cd889781b2c52d729567eecc87a86
SIN2WI <Natascha.Siebenhandl@at.bosch.com>
2024-07-29 15:12:06 +0200

    updated layout + added logo

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-document.getElementById('playButton').addEventListener('click', function() {
-    const audioFiles = [
-        'assets/audio/audio1.mp3',
-        'assets/audio/audio2.mp3',
-        'assets/audio/audio3.mp3',
-        'assets/audio/audio4.mp3',
-        'assets/audio/audio5.mp3'
-    ];
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('playButton');
+    const placeholder = document.querySelector('.button-placeholder');
 
-    const randomIndex = Math.floor(Math.random() * audioFiles.length);
-    const randomAudio = new Audio(audioFiles[randomIndex]);
-    randomAudio.play();
+    function showButtonAfterDelay() {
+        if (window.innerWidth < 800) {
+            button.style.fontSize = '1.5rem';
+            button.style.marginTop = '3.75rem';
+            button.style.padding = '1.25rem 3.75rem';
+            button.style.borderRadius = '0.5rem'; 
+        }
+
+        setTimeout(function() {
+            button.classList.remove('hidden');
+            placeholder.style.display = 'none'; 
+        }, 12000);
+    }
+
+    showButtonAfterDelay();
+
+    button.addEventListener('click', function() {
+        const audioFiles = [
+            'assets/audio/audio1.mp3',
+            'assets/audio/audio2.mp3',
+            'assets/audio/audio3.mp3',
+            'assets/audio/audio4.mp3',
+            'assets/audio/audio5.mp3'
+        ];
+
+        const randomIndex = Math.floor(Math.random() * audioFiles.length);
+        const randomAudio = new Audio(audioFiles[randomIndex]);
+        randomAudio.play();
+    });
 });
