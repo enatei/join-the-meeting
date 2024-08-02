b598e60f82fe3f8f634dea97472d636d69c9af6b
enatei <131289633+enatei@users.noreply.github.com>
2024-08-02 09:21:01 +0200

    updated/added audio files

--- script.js
+++ script.js
@@ -25,7 +25,8 @@ document.addEventListener('DOMContentLoaded', function() {
             'assets/audio/audio3.mp3',
             'assets/audio/audio4.mp3',
             'assets/audio/audio5.mp3',
-            'assets/audio/audio6.mp3'
+            'assets/audio/audio6.mp3',
+            'assets/audio/audio7.mp3'
         ];
 
         const randomIndex = Math.floor(Math.random() * audioFiles.length);
